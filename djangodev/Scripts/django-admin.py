8e356cb85dd171661d68a09b41af33f4d0a7c07e
hmeteor <yuyinghe2005@163.com>
2019-05-06 18:05:59 +0800

    Fixed #99999 -- Added a shortcut function to make toast.

---
+++ djangodev/Scripts/django-admin.py
@@ -0,0 +1,7 @@
+#!e:\gitsources\django\djangodev\scripts\python.exe
+# EASY-INSTALL-DEV-SCRIPT: 'Django==3.0.dev20190503115649','django-admin.py'
+__requires__ = 'Django==3.0.dev20190503115649'
+__import__('pkg_resources').require('Django==3.0.dev20190503115649')
+__file__ = 'E:\\gitsources\\django\\django\\bin\\django-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
